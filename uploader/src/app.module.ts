79c1ab6f3b74877cb5a0cef7ac670ce14f4445f7
Владислав Ярцев <stombie@yandex.ru>
2025-01-19 14:55:07 +0300

    remove uploader block

--- uploader/src/app.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from "@nestjs/common";
-import { VideoUploaderModule } from './video-uploader/video-uploader.module';
-@Module({
-  imports: [VideoUploaderModule]
-})
-export class AppModule {}
--- uploader/src/video-uploader/video-uploader.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { VideoUploaderService } from "./video-uploader.service";
-
-@Module({
-    providers: [VideoUploaderService],
-    exports: [VideoUploaderService]
-})
-export class VideoUploaderModule {}
