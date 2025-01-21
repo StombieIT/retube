3e1fa57947d4ba51d92c1ca61600dc32b90f9cd8
Владислав Ярцев <stombie@yandex.ru>
2025-01-22 01:27:45 +0300

    add connection to db from uploader

--- gateway/src/services/app.service.ts
+++ gateway/src/services/app.service.ts
@@ -1,15 +1,23 @@
 import { Repository } from 'typeorm';
 import * as fs from 'fs/promises';
-import { Flow, UploadSession, User, Video } from '@stombie/retube-core';
+import { Flow, IVideoChunk, UploadSession, User, Video } from '@stombie/retube-core';
 import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ChunkExchangeService } from './chunk-exchange.service';
 
 const MAX_CHUNK_LENGTH = 4096;
 
+interface Pusher {
+    chunkNumber: 0;
+    chunks: Buffer[];
+    sessionId: string;
+}
+
 @Injectable()
-export class AppService implements OnModuleInit, OnModuleDestroy {
+export class AppService implements OnModuleInit {
     private readonly logger = new Logger(AppService.name);
+    // TODO: remove
+    private readonly pusherById: Record<number, Pusher> = {};
 
     private video?: Video;
 
@@ -28,44 +36,76 @@ export class AppService implements OnModuleInit, OnModuleDestroy {
             size: 1500,
             content: Buffer.from(Array(1500).fill(1))
         };
-        await this.chunkExchange.push(chunk);
-        this.logger.log(`accept chunk: ${chunk.sessionId}`);
-        // this.logger.log(`Handled chunk ${chunk}`);
-        // const users = await this.usersRepository.find();
-        // if (!users.length) {
-        //     this.logger.error('No users found');
-        // }
-        // const user = users[0];
-        // const files = await Promise.all([
-        //     fs.readFile('/Users/vladislav.yartsev/Desktop/dev/personal/retube/gateway/src/test-video.mp4'),
-        //     fs.readFile('/Users/vladislav.yartsev/Desktop/dev/personal/retube/gateway/src/test-video_1.mp4'),
-        // ]);
-        // const flows = await Promise.all(files.map(async (file) => {
-        //     const flow = this.flowsRepository.create();
-        //     await this.flowsRepository.save(flow);
-        //     const uploadSession = this.uploadSessions.create({
-        //         total_bytes: file.length,
-        //         flow,
-        //     });
-        //     await this.uploadSessions.save(uploadSession);
-        //     return flow;
-        // }));
-        // this.video = this.videosRepository.create({
-        //     title: 'testing',
-        //     description: 'testing description',
-        //     duration: 10,
-        //     user,
-        //     flows,
-        // });
-        // await this.videosRepository.save(this.video);
-        // this.logger.log(`Created video ${this.video}`);
-
+        const users = await this.usersRepository.find();
+        if (!users.length) {
+            this.logger.error('No users found');
+        }
+        const user = users[0];
+        const files = await Promise.all([
+            fs.readFile('/Users/vladislav.yartsev/Desktop/dev/personal/retube/gateway/src/test-video.mp4'),
+            fs.readFile('/Users/vladislav.yartsev/Desktop/dev/personal/retube/gateway/src/test-video_1.mp4'),
+        ]);
+        const flows = await Promise.all(files.map(async (file) => {
+            const flow = this.flowsRepository.create();
+            await this.flowsRepository.save(flow);
+            const uploadSession = this.uploadSessions.create({
+                totalBytes: file.length,
+                flow,
+            });
+            await this.uploadSessions.save(uploadSession);
+            flow.uploadSession = uploadSession;
+            return flow;
+        }));
+        const sessionIds = flows.map(flow => flow.uploadSession.id);
+        this.video = this.videosRepository.create({
+            title: 'testing',
+            description: 'testing description',
+            duration: 10,
+            user,
+            flows,
+        });
+        
+        await this.videosRepository.save(this.video);
+        const chunksList = files.map(file => {
+            const chunkCount = Math.ceil(file.length / MAX_CHUNK_LENGTH);
+            const chunks = [];
+            for (let i = 0; i < chunkCount; i++) {
+                const chunkStart = i * MAX_CHUNK_LENGTH;
+                const chunkEnd = Math.min(
+                    chunkStart + MAX_CHUNK_LENGTH,
+                    file.length,
+                );
+                chunks.push(file.subarray(chunkStart, chunkEnd));
+            }
+            return chunks;
+        });
+        chunksList.forEach((chunks, idx) => {
+            this.pusherById[idx] = {
+                chunkNumber: 0,
+                chunks,
+                sessionId: sessionIds[idx],
+            };
+            this.initPushing(idx);
+        });
     }
 
-    async onModuleDestroy() {
-        // if (this.video) {
-        //     await this.videosRepository.remove(this.video);
-        //     this.logger.log(`Deleted video: ${this.video.id}`);
-        // }
+    async initPushing(id: number) {
+        const pusher = this.pusherById[id];
+        const { chunks, chunkNumber } = pusher;
+        const chunk = chunks[chunkNumber];
+        const chunkMessage: IVideoChunk = {
+            sessionId: pusher.sessionId,
+            content: chunk,
+            startByte: chunkNumber * MAX_CHUNK_LENGTH,
+            size: chunk.length,
+        };
+        await this.chunkExchange.push(chunkMessage);
+        console.log('handled');
+        setTimeout(() => {
+            pusher.chunkNumber++;
+            if (pusher.chunkNumber < pusher.chunks.length) {
+                this.initPushing(id);
+            }
+        }, 1000);
     }
 }
