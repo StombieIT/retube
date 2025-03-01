5384aa0b5c1fa2bd2d2b6093423bfeea221b61b8
Владислав Ярцев <stombie@yandex.ru>
2025-03-01 16:11:58 +0300

    implement scheduler enabling

--- uploader-v3/src/config/scheduler.config.ts
+++ uploader-v3/src/config/scheduler.config.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const SchedulerConfig = registerAs('scheduler', () => ({
+    enabled: process.env.SCHEDULER_ENABLED === 'true',
     sleepTime: parseInt(process.env.SCHEDULER_SLEEP_TIME, 10) || 60 * 1000,
     uploadingLifetimeMaximum: parseInt(process.env.SCHEDULER_LIFETIME_MAXIMUM, 10) || 10 * 60 * 1000,
 }));
--- uploader-v3/src/services/scheduler.service.ts
+++ uploader-v3/src/services/scheduler.service.ts
@@ -3,7 +3,6 @@ import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/commo
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Video, VideoStatus } from '@stombie/retube-core';
-import { ApiService } from './api.service';
 import { FFlowOrchestratorService } from './fflow-orchestrator.service';
 
 @Injectable()
@@ -11,33 +10,36 @@ export class SchedulerService implements OnModuleInit, OnModuleDestroy {
     private readonly logger = new Logger(SchedulerService.name);
     private readonly sleepTime: number;
     private readonly uploadingLifetimeMaximum: number;
+    private readonly enabled: boolean;
     private timer: NodeJS.Timeout;
 
     constructor(
         private readonly fflowOrchestrator: FFlowOrchestratorService,
-        private readonly api: ApiService,
         @InjectRepository(Video) private readonly videos: Repository<Video>,
         configService: ConfigService
     ) {
+        this.enabled = configService.get<boolean>('scheduler.enabled', false);
         this.sleepTime = configService.get<number>('scheduler.sleepTime', 60 * 1000);
         this.uploadingLifetimeMaximum = configService.get<number>('scheduler.uploadingLifetimeMaximum', 10 * 60 * 1000);
     }
 
-    getExpiredThreshold() {
-        return new Date(Date.now() - this.uploadingLifetimeMaximum);
-    }
-
     onModuleInit() {
-        this.tick();
+        if (this.enabled) {
+            this.tick();
+        }
     }
 
     onModuleDestroy() {
-        if (this.timer) {
+        if (this.enabled && this.timer) {
             clearTimeout(this.timer);
         }
     }
 
-    tick = async () => {
+    private getExpiredThreshold() {
+        return new Date(Date.now() - this.uploadingLifetimeMaximum);
+    }
+
+    private tick = async () => {
         this.logger.debug('Scheduler tick');
         const expiredThreshold = this.getExpiredThreshold();
         const where = {
