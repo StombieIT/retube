095efe73567c21417c5d5f678d3febcb161267ef
Владислав Ярцев <stombie@yandex.ru>
2025-04-02 21:24:39 +0300

    Revert "Добавил работу шедулера"
    This reverts commit b565f0a3847a0142d79099b1d5afac8b87d8c9e9.

--- uploader-v4/src/main/java/com/stombie/uploader_v4/UploaderV4Application.java
+++ uploader-v4/src/main/java/com/stombie/uploader_v4/UploaderV4Application.java
@@ -2,10 +2,8 @@ package com.stombie.uploader_v4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class UploaderV4Application {
 
 	public static void main(String[] args) {
