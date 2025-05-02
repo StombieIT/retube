12b381c66318f5d0e39938de6afd0c097b454bf7
Владислав Ярцев <stombie@yandex.ru>
2025-05-03 01:07:14 +0300

    partly implement synchronizing tests

--- e2e-testing/pages/sync-editor/synchronizing-page.ts
+++ e2e-testing/pages/sync-editor/synchronizing-page.ts
@@ -1,8 +1,81 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { AuthorizedPage } from './authorized-page';
 
+const TITLE_PLACEHOLDER = 'Заголовок видео';
+const DESCRIPTION_PLACEHOLDER = 'Описание видео';
+const ADD_FLOW_BUTTON_LABEL = 'Добавить поток';
+const UPLOAD_BUTTON_LABEL = 'Загрузить';
+const DELETE_BUTTON_LABEL = 'Удалить';
+const MOTIVATION_TEXT_TEST_ID = 'motivation-text';
+const FLOW_CANDIDATE_TEST_ID = 'flow-candidate';
+const FLOWS_GRID_TEST_ID = 'flows-grid';
+const INPUT_FILE_SELECTOR = 'input[type="file"]';
+const VIDEO_SELECTOR = 'video';
+
 export class SynchronizingPage extends AuthorizedPage {
+    private readonly titleField: Locator;    
+    private readonly descriptionField: Locator;    
+
     constructor(page: Page) {
         super(page);
+        this.titleField = this.page.getByPlaceholder(TITLE_PLACEHOLDER);
+        this.descriptionField = this.page.getByPlaceholder(DESCRIPTION_PLACEHOLDER);
+    }
+
+    getMotivationText() {
+        return this.page.getByTestId(MOTIVATION_TEXT_TEST_ID);
+    }
+
+    getFlowsGrid() {
+        return this.page.getByTestId(FLOWS_GRID_TEST_ID);
+    }
+
+    getVideoLocator() {
+        return this.page.locator(VIDEO_SELECTOR);
+    }
+
+    async fillTitle(title: string) {
+        await this.titleField.fill(title);
+    }
+
+    async fillDescription(description: string) {
+        await this.descriptionField.fill(description);
+    }
+
+    async addFlow() {
+        const addFlowButton = this.page.getByRole('button', { name: ADD_FLOW_BUTTON_LABEL });
+        await this.checkDisabled(addFlowButton);
+        await addFlowButton.click();
+    }
+
+    async addVideoToFlow(flowIdx: number, video: Buffer) {
+        await this.page.getByTestId(FLOW_CANDIDATE_TEST_ID)
+            .nth(flowIdx)
+            .locator(INPUT_FILE_SELECTOR)
+            .setInputFiles({
+                name: `flow-${flowIdx}.mp4`,
+                mimeType: 'video/mp4',
+                buffer: video,
+            });
+    }
+
+    async deleteFlow(flowIdx: number) {
+        await this.page.getByTestId(FLOW_CANDIDATE_TEST_ID)
+            .nth(flowIdx)
+            .getByRole('button', { name: DELETE_BUTTON_LABEL })
+            .click();
+    }
+
+    async uploadFlow() {
+        const uploadButton = this.page.getByRole('button', { name: UPLOAD_BUTTON_LABEL });
+        await this.checkDisabled(uploadButton);
+        await this.page.getByRole('button', { name: UPLOAD_BUTTON_LABEL }).click();
+    }
+
+    private async checkDisabled(locator: Locator) {
+        const isDisabled = await locator.isDisabled();
+        if (isDisabled) {
+            throw new Error('Элемент не активен');
+        }
     }
 }
---
+++ e2e-testing/tests/sync-editor/synchronizing.spec.ts
@@ -0,0 +1,82 @@
+import { expect, test } from '@playwright/test';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { AuthModal } from '../../pages/sync-editor/auth-modal';
+import { SynchronizingPage } from '../../pages/sync-editor/synchronizing-page';
+
+const {
+    USER_EMAIL,
+    USER_PASSWORD,
+} = process.env;
+
+const MAX_FLOWS_COUNT = 4;
+const UPLOADING_MAX_FLOWS_COUNT = 2;
+
+test.describe('Страница синхронизации', () => {
+    let videoBuffer: Buffer;
+
+    test.beforeAll(async () => {
+        videoBuffer = await fs.readFile(
+            path.resolve(__dirname, 'data', 'test-video.mp4')
+        );
+    });
+
+    test.beforeEach(async ({ page }) => {
+        const authModal = new AuthModal(page);
+
+        await authModal.open();
+        await authModal.switchToLogIn();
+        await authModal.fillEmail(USER_EMAIL!);
+        await authModal.fillPassword(USER_PASSWORD!);
+        await authModal.submit();
+    });    
+
+    test('ui сетки потоков', async ({ page }) => {
+        const synchronizing = new SynchronizingPage(page);
+
+        for (let i = 0; i < MAX_FLOWS_COUNT; i++) {
+            await synchronizing.addFlow();
+            const flowGrid = synchronizing.getFlowsGrid();
+            await expect(flowGrid).toHaveScreenshot(`flow_grid_${i + 1}.png`);
+        }
+    });
+
+    test('ограничение на количество потоков', async ({ page }) => {
+        const synchronizing = new SynchronizingPage(page);        
+
+        for (let i = 0; i < MAX_FLOWS_COUNT; i++) {
+            await synchronizing.addFlow();
+        }
+
+        await expect(synchronizing.addFlow).rejects.toThrow();
+    });
+
+    // test('добавление и удаление потоков', () => {
+    // });
+
+    test('загрузка видео в потоки', async ({ page }) => {
+        const synchronizing = new SynchronizingPage(page);
+
+        for (let i = 0; i < UPLOADING_MAX_FLOWS_COUNT; i++) {
+            await synchronizing.addFlow();
+        }
+
+        const flowsGrid = synchronizing.getFlowsGrid();
+
+        await expect(flowsGrid).toHaveScreenshot('uploading_grid_init.png');
+
+        for (let i = 0; i < UPLOADING_MAX_FLOWS_COUNT; i++) {
+
+            await synchronizing.addVideoToFlow(i, videoBuffer);
+            
+            await expect(flowsGrid).toHaveScreenshot(`uploading_grid_${i + 1}.png`, {
+                mask: [
+                    synchronizing.getVideoLocator(),
+                ]
+            });
+        }
+    });
+
+    // test('валидация перед загрузкой', () => {
+    // });
+});
